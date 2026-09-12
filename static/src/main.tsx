33480c5bd3f3e54c0011ad3301251fce13934341
luisgrasso <grasso.luis@gmail.com>
2026-09-12 20:14:10 -0300

    feat(hero): load fullscreen hero treatment

--- static/src/main.tsx
+++ static/src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './styles/hero-fullscreen.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
